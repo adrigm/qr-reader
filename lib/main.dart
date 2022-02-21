1a58ad3b91395d333c63e9026834b52597fe2367
adrigm <adrigm.admin@gmail.com>
2022-02-21 17:31:14 +0100

    created screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/screens/home_screen.dart';
+import 'package:qr_reader/screens/map_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,17 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'map': (_) => const MapScreen()
+      },
     );
   }
 }
